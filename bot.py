879123a1b717099b7cd66c23606fc279a43879b4
Joshua Vial <joshua@enspiral.com>
2023-06-19 13:32:06 +1200

    post most recent video on connect

--- bot.py
+++ bot.py
@@ -1,19 +1,33 @@
 import os
 import discord
-from discord import Intents
+from googleapiclient.discovery import build
 from dotenv import load_dotenv
 
 load_dotenv()
 discord_token = os.getenv('DISCORD_TOKEN')
+announcement_channel_id = os.getenv('ANNOUNCEMENT_CHANNEL_ID')
+youtube_api_key = os.getenv('YOUTUBE_API_KEY')
+youtube_channel_id = os.getenv('YOUTUBE_CHANNEL_ID')
 
 if discord_token is None:
     print('Error: DISCORD_TOKEN environment variable is not set.')
     exit(1)
 
-intents = Intents.default()
+if announcement_channel_id is None:
+    print('Error: ANNOUNCEMENT_CHANNEL_ID environment variable is not set.')
+    exit(1)
+
+if youtube_api_key is None:
+    print('Error: YOUTUBE_API_KEY environment variable is not set.')
+    exit(1)
+
+if youtube_channel_id is None:
+    print('Error: YOUTUBE_CHANNEL_ID environment variable is not set.')
+    exit(1)
+
+intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
-intents.messages = True  # Enable messaging intent
 
 bot = discord.Client(intents=intents)
 
@@ -22,12 +36,23 @@ async def on_ready():
     print(f'Logged in as {bot.user.name}')
     print('Bot is ready to receive messages')
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+    youtube = build('youtube', 'v3', developerKey=youtube_api_key)
+    request = youtube.search().list(
+        part='snippet',
+        channelId=youtube_channel_id,
+        maxResults=1,
+        order='date',
+        type='video'
+    )
+    response = request.execute()
 
-    if message.content.lower() == 'hello':
-        await message.channel.send('Hello, World!')
+    videos = response.get('items', [])
+    if videos:
+        video = videos[0]
+        video_id = video['id']['videoId']
+        video_url = f'https://www.youtube.com/watch?v={video_id}'
+
+        announcement_channel = bot.get_channel(int(announcement_channel_id))
+        await announcement_channel.send(f'New video published: {video_url}')
 
 bot.run(discord_token)
