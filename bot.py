1a2ba9eafb0d337f692777d9a839ae50a37d642b
Joshua Vial <joshua@enspiral.com>
2023-06-19 13:41:28 +1200

    periodically check for updates and post

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os
 import discord
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
+import time
 
 load_dotenv()
 discord_token = os.getenv('DISCORD_TOKEN')
@@ -31,11 +33,7 @@ intents.presences = False
 
 bot = discord.Client(intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-    print('Bot is ready to receive messages')
-
+async def check_new_video():
     youtube = build('youtube', 'v3', developerKey=youtube_api_key)
     request = youtube.search().list(
         part='snippet',
@@ -51,8 +49,32 @@ async def on_ready():
         video = videos[0]
         video_id = video['id']['videoId']
         video_url = f'https://www.youtube.com/watch?v={video_id}'
+        if not is_same_url(video_url):
+            announcement_channel = bot.get_channel(int(announcement_channel_id))
+            await announcement_channel.send(f'New video published: {video_url}')
+            save_url(video_url)
 
-        announcement_channel = bot.get_channel(int(announcement_channel_id))
-        await announcement_channel.send(f'New video published: {video_url}')
+def is_same_url(video_url):
+    file_path = '.most-recent-url'
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            last_url = file.read().strip()
+        return last_url == video_url
+    return False
+
+def save_url(video_url):
+    file_path = '.most-recent-url'
+    with open(file_path, 'w') as file:
+        file.write(video_url)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    print('Bot is ready to receive messages')
+
+    while True:
+        await check_new_video()
+        # Wait for 15 minutes before checking again
+        await discord.utils.sleep_until(datetime.now() + timedelta(minutes=15))
 
 bot.run(discord_token)
